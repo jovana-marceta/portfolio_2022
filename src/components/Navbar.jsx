61817e22344df6596607993ae5048b0a4b5286ce
Jovana Marceta <jmarcetafedev@gmail.com>
2022-06-19 18:03:20 +0200

    Adding linkedin link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a className='flex justify-between items-center w-full text-gray-300'
-               href="www.linkedin.com/in/jovanamarceta" target='_blank'>
+               href="https://www.linkedin.com/in/jovanamarceta/</li>" target='_blank'>
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
